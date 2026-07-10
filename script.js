6c8f3f5691740d08a7af1d74150fe8503d3a9553
marcopmorocho17-afk <marcop.morocho17@gmail.com>
2026-07-10 14:11:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -595,3 +595,5 @@ window.prepararEdicionModular = prepararEdicionModular;
 window.resetearFormularioModular = resetearFormularioModular;
 window.listarModularesAdmin = listarModularesAdmin;
 window.eliminarProductoModular = eliminarProductoModular;
+// Añade esta línea al puro fondo de tu script de administración junto con las otras vinculaciones
+window.eliminarFotoTradicionalPorId = eliminarFotoTradicionalPorId;
