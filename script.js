f69623d91f6b9798298b8832d945b4043b31c59b
marcopmorocho17-afk <marcop.morocho17@gmail.com>
2026-07-10 09:06:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const ADMIN_PASSWORD = "Maderas_Habitarq_2026";
 const SUPABASE_URL = "https://mpjwdgekznvukmpprlat.supabase.co"; 
 const SUPABASE_ANON_KEY = "sb_publishable_lnwuBk9887iZy76uvAxeIQ_8StvDZ8K";
 
+
 // Mini-librería con aislamiento nativo estable contra errores CORS
 (function(global) {
     function SupabaseClient(urlProyecto, keyProyecto) {
